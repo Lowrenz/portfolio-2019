4be54029d90542647d3b885b21906e17ba2dba0a
Lorenz Gillisjans <lorenz.gillisjans@continuous.be>
2018-11-22 23:08:19 +0100

    Stripped old shizzle + added GA

--- web/app/themes/Low_res/sections/section-skills.php
+++ web/app/themes/Low_res/sections/section-skills.php
@@ -14,14 +14,4 @@ $page_id = get_the_ID();
       </div>
       <?php if(current_user_can('administrator')) edit_post_link('edit section', '<p class="edit-post">', '</p>',$page_id); ?>
     </div>
-    <div class="row">
-      <div class="small-12 medium-7 columns centered">
-        <div class="companies-detail mobile text-center">
-          <?php the_title( '<h2 class="title title--stripe banner__title">', '</h2>' );  ?>
-          <div class="banner__content">
-            <?php the_content(); ?>
-          </div>
-        </div>
-      </div>
-    </div>
   </section>
