b777fbbdeac5ecc5cf903e760cecd1f53810f074
Lorenz Gillisjans <lorenz.gillisjans@continuous.be>
2018-11-28 21:34:06 +0100

    Extra scss separation + hero & nav updates

--- web/app/themes/Low_res/sections/section-hero.php
+++ web/app/themes/Low_res/sections/section-hero.php
@@ -5,20 +5,25 @@
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 ?>
 
-<section id="home" class="section section--hero fullpage">
+<section id="home" class="section section--hero fullpage" data-aos="fade-up" data-aos-duration="1500">
   <div class="hero-background" style="background-image: url('<?php echo $thumb['0']; ?>')"></div>
   <div class="row">
-    <div class="small-12 large-6 columns">
-      <img class="site-logo align-self-center" src="<?php echo get_template_directory_uri(); ?>/dist/img/lowres-logo.svg"
-        data-aos="fade-up" data-aos-duration="1500">
-    </div>
-  </div>
-  <div class="row">
-    <div class="small-12 columns">
-      <div class="hero-content">
-        <h2 class="alien align-self-center text-center">
-          <?php the_content(); ?>
-        </h2>
+    <div class="column algin-self-middle align-center">
+      <div class="row">
+        <div class="small-12 columns">
+          <div class="logo-wrapper">
+            <img class="site-logo pulsate-fwd" src="<?php echo get_template_directory_uri(); ?>/dist/img/lowres-logo.svg">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-12 columns">
+          <div class="hero-content">
+            <h2 class="alien align-self-center text-center">
+              <?php the_content(); ?>
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   </div>
